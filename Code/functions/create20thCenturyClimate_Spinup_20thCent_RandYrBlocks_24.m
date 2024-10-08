72d47e1ad10005f093f4deae711f4af30457dcca
Alejandro <afernandez@twodegreesadapt.com>
2024-10-08 15:48:05 -0500

    monte carlo code added with dependencies

---
+++ Code/functions/create20thCenturyClimate_Spinup_20thCent_RandYrBlocks_24.m
@@ -0,0 +1,83 @@
+function climate=create20thCenturyClimate_Spinup_20thCent_RandYrBlocks_24(n_spinupyears,n_reps)
+
+%load('G:\My Drive\lake_model\LakeModel2\FinalClimateCompilation\climateCompilation20thCent')
+load('climateCompilation20thCent_24.mat')
+
+%load('G:\My Drive\lake_model\LakeModel2\Castor\climate_avgYr.mat')
+load('climate_avgYr.mat')
+
+%% fill RH
+%use model to predict rh
+predicted_rh_uncert=normrnd(predicted_rh,(predicted_rh_ci(:,2)-predicted_rh_ci(:,1))/2);
+predicted_rh_uncert(predicted_rh>100)=100;
+rh_20thCent=[predicted_rh_uncert;agrimetcastor_rh(:,4)];
+climate_20thCent=[final_pt,rh_20thCent];
+
+%% fill Rs data
+%create 20th century insolation (Rs) by looping measured data
+
+Rs20thCentloopedwholes=readmatrix('Rs_20thCent_looped_w_holes_24.csv');
+Rs=fillmissing(Rs20thCentloopedwholes(:,4),'linear');
+climate_20thCent(:,7)=Rs;
+
+%% fill Tw
+%use model to predict Tw
+predicted_tw_uncert=normrnd(predicted_tw,(predicted_tw_ci(:,2)-predicted_tw_ci(:,1))/2);
+predicted_tw_uncert(predicted_tw<1)=1;
+
+Tw_20thCent=[predicted_tw_uncert;castor_tw(:,4)];
+climate_20thCent(:,8)=Tw_20thCent;
+%% create spin-up time avg climate data 
+spinupyears=(climate_20thCent(1,3)-n_spinupyears : climate_20thCent(1,3)-1)';
+dates_spinup=(datetime(spinupyears(1),1,1):datetime(climate_20thCent(1,3),climate_20thCent(1,2),climate_20thCent(1,1))-1)';
+
+climate_spinup=NaN(size(dates_spinup,1),8);
+climate_spinup(:,1)=day(dates_spinup);
+climate_spinup(:,2)=month(dates_spinup);
+climate_spinup(:,3)=year(dates_spinup);
+j=1;
+
+for i = dates_spinup(1):dates_spinup(end)
+    
+    climate_spinup(j,4)=climate_avg(climate_avg(:,1)==day(i) & climate_avg(:,2)==month(i),4);
+    climate_spinup(j,5)=climate_avg(climate_avg(:,1)==day(i) & climate_avg(:,2)==month(i),5);
+    climate_spinup(j,6)=climate_avg(climate_avg(:,1)==day(i) & climate_avg(:,2)==month(i),6);
+    climate_spinup(j,7)=climate_avg(climate_avg(:,1)==day(i) & climate_avg(:,2)==month(i),7);
+    climate_spinup(j,8)=climate_avg(climate_avg(:,1)==day(i) & climate_avg(:,2)==month(i),8);
+    j=j+1;
+end
+
+climate_20thCent=climate_20thCent(climate_20thCent(:,3) > climate_20thCent(1,3) &...
+    climate_20thCent(:,3) < max(climate_20thCent(:,3)),:);
+
+%% re-order climate dataset in 10yr blocks
+blocks=[1900 1909; 1910 1919; 1920 1929;1930 1939;1940 1949;1950 1959;...
+    1960 1969;1970 1979;1980 1989;1990 1999;2000 2009;2010 2018];
+
+blocks_rand = blocks(randperm(size(blocks,1)),:);
+blocks_rand = repmat(blocks_rand,n_reps,1);
+temp=NaN(1,8);
+
+for i=1:length(blocks_rand)
+    block_i_lo=blocks_rand(i,1);
+    block_i_hi=blocks_rand(i,2);
+    clim_block=climate_20thCent(climate_20thCent(:,3) >= block_i_lo ...
+        & climate_20thCent(:,3) <= block_i_hi,:);
+    temp=[temp;clim_block];
+end
+
+temp(1,:)=[];
+clim_blocks=temp;
+clim_blocks(1,3)=climate_20thCent(end,3)+1;
+yr_change=find(clim_blocks(:,1)==1 & clim_blocks(:,2)==1);
+
+for ii=1:size(yr_change,1)
+    if ii<size(yr_change,1)
+        clim_blocks(yr_change(ii):yr_change(ii+1)-1,3)=ii+climate_20thCent(end,3);
+    else
+        clim_blocks(yr_change(ii):end,3)=ii+climate_20thCent(end,3);
+    end
+% clim_blocks(:,3)=repmat(climate_20thCent(:,3),2,1)
+climate=[climate_spinup;climate_20thCent;clim_blocks];
+end
+end
---
+++ Code/model_montecarlo_reconstruc.m
@@ -0,0 +1,219 @@
+%% monte carlo simulations
+clear
+tic
+% created from 20th century uncertainty calibration in
+% calibrationResults20thCentUncertCalib.m
+% load('G:\My Drive\lake_model\LakeModel2\Castor\uncertainty_final.mat');
+load('../../Data/uncertainty_final.mat');
+
+
+clim=create20thCenturyClimate_Spinup_24();
+final_20thCentYear=clim(end,3);
+years_clim=(clim(1,3):clim(end,3))';
+n_spinupyears=20; %number of years average climate spinup of the model
+n_reps=2; %number of times the 20th Cent dataset will be permutated in its re-ordered version by 10yr blocks
+
+climate=create20thCenturyClimate_Spinup_20thCent_RandYrBlocks_24(n_spinupyears,n_reps);
+    
+results=struct;
+ni_MC = 1000;
+a=2.45;
+b=38.03;
+C_IN=0.001;
+AWC_mod=1.87;
+
+P_summer_range=[-0.4    0.4];
+P_winter_range=[-0.4    0.0];
+T_mod_summer_range=[0 2];
+T_mod_winter_range=[-3 1];
+RH_range=[-0.3  0.0];
+
+P_mod_summer_mu=mean(P_summer_range);
+P_mod_summer_sigma=(P_summer_range(2)-P_mod_summer_mu)/2;
+
+P_mod_winter_mu=mean(P_winter_range);
+P_mod_winter_sigma=(P_winter_range(2)-P_mod_winter_mu)/2;
+
+T_mod_summer_mu=mean(T_mod_summer_range);
+T_mod_summer_sigma=(T_mod_summer_range(2)-T_mod_summer_mu)/2;
+
+T_mod_winter_mu=mean(T_mod_winter_range);
+T_mod_winter_sigma=(T_mod_winter_range(2)-T_mod_winter_mu)/2;
+
+RH_mod_mu=mean(RH_range);
+RH_mod_sigma=(RH_range(2)-RH_mod_mu)/2;
+
+results.MD_max_v=NaN(1,ni_MC);
+results.summer_begin_v=NaN(1,ni_MC);
+results.summer_len_v=NaN(1,ni_MC);
+results.P_mod_summer_v=NaN(1,ni_MC);
+results.P_mod_winter_v=NaN(1,ni_MC);
+results.T_mod_summer_v=NaN(1,ni_MC);
+results.T_mod_winter_v=NaN(1,ni_MC);
+results.RH_mod_v=NaN(1,ni_MC);
+
+results.year_summer_arag=NaN(size((climate(1,3):climate(end,3))',1),ni_MC);
+results.trail_avg_year_summer_arag=NaN(size((climate(1,3):climate(end,3))',1),ni_MC);
+
+results.obs_coefficients=NaN(1,ni_MC); %intercept row 1, %slope row 2
+results.obs_corr=NaN(1,ni_MC);
+results.obs_rmse=NaN(1,ni_MC);
+results.obs_mean_diff=NaN(1,ni_MC);
+
+results.samples_rmse=NaN(1,ni_MC);
+
+results.core_corr=NaN(1,ni_MC);
+results.core_rmse=NaN(1,ni_MC);
+results.core_rmse_mm=NaN(1,ni_MC);
+
+results.meanCore_meanYearArag_diff=NaN(1,ni_MC);
+
+for i_MC = 1 : ni_MC
+        
+    MD_max=MD_new_mean+randn*MD_new_std;
+    if MD_max <= 10
+        MD_max=10;
+    end
+    results.MD_max_v(i_MC)=MD_max;
+    
+    summer_begin=floor(summer_begin_new_mean+randn*summer_begin_new_std);
+    if summer_begin < 105
+        summer_begin=105;
+    end
+    if summer_begin > 181
+        summer_begin=181;
+    end
+    results.summer_begin_v(i_MC)=summer_begin;
+    
+    summer_len=floor(summer_len_new_mean+randn*summer_len_new_std);
+    results.summer_len_v(i_MC)=summer_len;
+    
+    P_mod_summer=(P_mod_summer_mu+randn*P_mod_summer_sigma);
+    results.P_mod_summer_v(i_MC)=P_mod_summer;
+    
+    P_mod_winter=(P_mod_winter_mu+randn*P_mod_winter_sigma);
+    results.P_mod_winter_v(i_MC)=P_mod_winter;
+    
+    T_mod_summer=T_mod_summer_mu+randn*T_mod_summer_sigma;
+    results.T_mod_summer_v(i_MC)=T_mod_summer;
+    
+    T_mod_winter=T_mod_winter_mu+randn*T_mod_winter_sigma;
+    results.T_mod_winter_v(i_MC)=T_mod_winter;
+    
+    RH_mod=(RH_mod_mu+randn*RH_mod_sigma);
+    results.RH_mod_v(i_MC)=RH_mod;
+    
+    yearly_P_seasonality=calc_yearly_P_seasonality_indep(P_mod_summer,P_mod_winter,climate);
+    ModelCastorContinuousClimDaily_20thCenturyClimMC_ReconstLoops;
+    
+    results.daily_LD_mm(:,i_MC)=daily_LDv_mm;
+    results.daily_dl(:,i_MC)=daily_dl;
+    
+    if size(year_summer_arag,1)==size(results.trail_avg_year_summer_arag,1)
+        results.year_summer_arag(:,i_MC)=year_summer_arag;
+        results.trail_avg_year_summer_arag(:,i_MC)=trail_avg_year_summer_arag;
+    else 
+        results.trail_avg_year_summer_arag(:,i_MC)=NaN;
+    end
+    
+    
+    if bad_run==false
+        
+        results.obs_coefficients(1:2,i_MC)=lm_obs.Coefficients.Estimate;
+        results.obs_corr(i_MC)=obs_corr;
+        results.obs_rmse(i_MC)=obs_rmse;
+        results.obs_mean_diff(i_MC)=obs_mean_diff;
+
+        results.samples_rmse(i_MC)=obs_rmse;
+
+        results.core_corr(i_MC)=core_corr;
+        results.core_rmse(i_MC)=core_rmse;
+        results.meanCore_meanYearArag_diff(i_MC)=meanCore_meanYearArag_diff;
+    end
+    
+    i_MC
+
+    climate=create20thCenturyClimate_Spinup_20thCent_RandYrBlocks_24(n_spinupyears,n_reps);
+end
+
+total_years=size(results.trail_avg_year_summer_arag,1);
+total_years_v=(1:total_years)';
+results.dates=dates_clim;
+results.dates_years=dates_years;
+yearsi=year(dates_years);
+
+results.mean_arag_spinup=mean(results.year_summer_arag(1:n_spinupyears,:),1);
+% results.mean_arag_20thCent=mean(results.year_arag(n_spinupyears+20:end,:),1);
+results.mean_arag_20thCent=mean(results.year_summer_arag(n_spinupyears+20:find(yearsi==2018),:),1);
+results.mean_arag_no_mod=mean(results.year_summer_arag(find(yearsi==2018)+20:find(yearsi==floor(mean([2018 2256]))),:),1);
+results.mean_arag_mod=mean(results.year_summer_arag(find(yearsi==floor(mean([2018 2256])))+20:end,:),1);
+
+%% stuff for naming file
+
+
+filename=strcat('../../Results/MCres_recons_ClimMDaragTiming'...
+    ,datestr(now,'_ddmmmm_yyyy_HH MM'),'_i=',num2str(ni_MC),'.mat');
+
+
+save(filename,'results','-v7.3');
+toc
+
+%% reconstruction
+load(filename)
+
+goal=-2.4;
+uncertainty=0.2;
+positions=find(results.mean_arag_mod < goal+uncertainty & results.mean_arag_mod > goal-uncertainty);
+arag_recons=results.mean_arag_mod(positions);
+MD_recons=results.MD_max_v(positions);
+summer_begin_recons=results.summer_begin_v(positions);
+summer_len_recons=results.summer_len_v(positions);
+P_mod_summer_recons=results.P_mod_summer_v(positions);
+P_mod_winter_recons=results.P_mod_winter_v(positions);
+T_mod_summer_recons=results.T_mod_summer_v(positions);
+T_mod_winter_recons=results.T_mod_winter_v(positions);
+RH_mod_recons=results.RH_mod_v(positions);
+
+figure
+subplot(4,1,1)
+histogram(results.mean_arag_mod,[-8:0.2:0])
+title('All arag results')
+
+subplot(4,1,2)
+histogram(MD_recons)
+title('MD reconstruction values')
+
+subplot(4,1,3)
+histogram(summer_begin_recons)
+title('beginning of summer')
+
+
+subplot(4,1,4)
+histogram(summer_len_recons)
+title('Length of summer')
+
+figure
+subplot(4,1,1)
+ksdensity(P_mod_summer_recons)
+xlim([-0.5 0.5])
+title('Summer precip')
+
+subplot(5,1,2)
+ksdensity(P_mod_winter_recons)
+xlim([-0.5 0.5])
+title('Winter precip')
+
+subplot(5,1,3)
+ksdensity(T_mod_summer_recons)
+xlim([-4 4])
+title('Temp summer')
+
+subplot(5,1,4)
+ksdensity(T_mod_winter_recons)
+xlim([-4 4])
+title('Temp winter')
+
+subplot(5,1,5)
+ksdensity(RH_mod_recons)
+xlim([-0.5 0.5])
+title('RH')
